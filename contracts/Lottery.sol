26d3e17a3604cd83a61e1b984b834084dc3fdf30
IntegralTeam <repo@sfxdx.ru>
2021-04-28 02:02:31 +0300

    use oni names
    Signed-off-by: IntegralTeam <repo@sfxdx.ru>

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/proxy/Initializable.sol";
 
 import "./LotteryNFT.sol";
